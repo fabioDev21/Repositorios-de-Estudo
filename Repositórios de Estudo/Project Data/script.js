e96d102d2ca55f7000928b1f3a24b0778c5b4413
peije21 <fabiotwist1@gmail.com>
2023-12-28 23:03:55 -0300

    Adicionando Projeto Data

---
+++ Repositórios de Estudo/Project Data/script.js
@@ -0,0 +1,6 @@
+    let instanciaData = new Date
+    let pegaDia = instanciaData.getDate
+    let pegaMes = instanciaData.getMonth
+    let pegaAno = instanciaData.getFullYear
+    
+    console.log(pegaDia,pegaMes,pegaAno )
