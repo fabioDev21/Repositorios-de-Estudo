2d798c556f9d17b4e7d02fd81ea3b4991e550a4b
peije21 <fabiotwist1@gmail.com>
2023-12-29 20:11:40 -0300

    mudanças e teste

--- Repositórios de Estudo/ProjectData/script.js
+++
@@ -1,6 +0,0 @@
-    let instanciaData = new Date
-    let pegaDia = instanciaData.getDate
-    let pegaMes = instanciaData.getMonth
-    let pegaAno = instanciaData.getFullYear
-    
-    console.log(pegaDia,pegaMes,pegaAno )
